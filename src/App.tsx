cd611fc75b164e3b6769d9a59d41e65e213f1936
ritikapurwa08 <8ballpoolrk2@gmail.com>
2026-01-06 16:58:14 +0530

    ui-htmls-pages-added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 export default function App(){
   return (
     <div>
-      <h1>
-        hlo how are you 
+      <h1 className={"font-lexend font-bold"}>
+        hlo how are you
+        this
+        <p></p>
+        <h1 className="justify-center ">
+<div className="font-lexend">
+      <h1 className="text-2xl font-bold">This is Lexend Font</h1>
+      <p>Your content will now use the Lexend typeface.</p>
+    </div>
+        </h1>
       </h1>
     </div>
   )
